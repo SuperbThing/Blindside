e8a2825f2895887f39594d580b91ba9c409b8c02
spencers145 <smartypantsspencer@gmail.com>
2025-12-26 21:39:58 -0500

    Fix Checker Literally just Not Being in the Pool at all??

--- trinkets/checker.lua
+++ trinkets/checker.lua
@@ -12,6 +12,7 @@
         in_pool = function(self, args)
             if G.GAME.selected_back.effect.center.config.extra then
                 if not G.GAME.selected_back.effect.center.config.extra.blindside then return false end
+                return true
             else
             return false
             end
