0ee61a35513091c9c74135f0469b0f14391338d6
spencers145 <smartypantsspencer@gmail.com>
2025-12-29 07:36:30 -0500

    Fix The Bell triggering twice

--- enhancements/Bell.lua
+++ enhancements/Bell.lua
@@ -21,19 +21,19 @@
             end
         end,
         calculate = function(self, card, context)
-        if context.discard and context.other_card == card and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
-            G.E_MANAGER:add_event(Event({
-                trigger = 'before',
-                delay = 0.0,
-                func = function()
-                    SMODS.add_card({ set = 'bld_obj_filmcard' })
-                    G.GAME.consumeable_buffer = 0
-                    return true
-                end
-            }))
-            return { message = localize('k_filmcard_ex'), colour = G.C.SECONDARY_SET.bld_obj_filmcard, burn = not card.ability.extra.upgraded }
-        end
+            if context.discard and context.main_eval and context.other_card == card and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'before',
+                    delay = 0.0,
+                    func = function()
+                        SMODS.add_card({ set = 'bld_obj_filmcard' })
+                        G.GAME.consumeable_buffer = 0
+                        return true
+                    end
+                }))
+                return { message = localize('k_filmcard_ex'), colour = G.C.SECONDARY_SET.bld_obj_filmcard, burn = not card.ability.extra.upgraded }
+            end
         end,
         loc_vars = function(self, info_queue, card)
             if not card.ability.extra.upgraded then
